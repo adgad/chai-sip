b0185beccc64de3592fa9e375e32729adece7ce8
Arjun Gadhia <arjungadhia@gmail.com>
2018-09-13 02:04:28 +0100

    Make logo a link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,9 @@ import 'logo.sass'
 const Logo = ({ tagline }) => (
 	<div className="logo">
 		<h1 className="title visually-hidden">Chai Sip</h1>
-		<img className="logo-image" src="/img/logo.jpeg" alt=""/>
+		<a className="logo-image" href="/">
+			<img className="logo-image" src="/img/logo.jpeg" alt=""/>
+		</a>
 		<div className="logo-tagline">
 			<span class="logo-tagline-text">{tagline}</span>
 		</div>
