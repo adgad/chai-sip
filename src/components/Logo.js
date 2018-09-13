705267f74604e96b7b703d6b08b654a63da5b1f6
Arjun Gadhia <arjungadhia@gmail.com>
2018-09-13 01:32:53 +0100

    More logo styling

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const Logo = () => (
-	<h1 className="title">Chai Sip</h1>
+import 'logo.sass'
+
+const Logo = ({ data }) => (
+	<div className="logo">
+		<h1 className="title visually-hidden">Chai Sip</h1>
+		<img className="logo-image" src="/img/logo.jpeg" alt=""/>
+		<div className="logo-tagline">
+			<span class="logo-tagline-text">Bring a richer tea experience to your next event.</span>
+		</div>
+	</div>
 )
 
 export default Logo;
