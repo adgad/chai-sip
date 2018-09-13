97bff84f8b54a19a52e954a0981fe88174c329d4
Arjun Gadhia <arjungadhia@gmail.com>
2018-09-14 00:45:27 +0100

    a11y for logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ import 'logo.sass'
 const Logo = ({ tagline }) => (
 	<div className="logo">
 		<h1 className="title visually-hidden">Chai Sip</h1>
-		<a className="logo-image" href="/" alt="Home">
+		<a className="logo-image" href="/" aria-hidden>
 			<img className="logo-image" src="/img/logo.jpeg" alt=""/>
 		</a>
 		<div className="logo-tagline">
